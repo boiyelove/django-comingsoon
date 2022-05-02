e36d6fe5b054d8c334563361faac1a8dcc368877
boiyelove <daahrmmieboiye@gmail.com>
2022-05-02 18:50:33 +0100

    added static files

--- apps.py
+++ apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class ComingsoonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'comingsoon'
+
+    def ready(self):
+        import os
+        from django.conf import settings
+        # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        # print("BASE_DIR", BASE_DIR)
+        settings.STATICFILES_DIRS += (os.path.join(BASE_DIR, 'comingsoon/static/'),)
